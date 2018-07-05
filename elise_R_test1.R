27fc1a8b33703a12e0633fdb5d1c1e13cbe9ef55
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2018-07-05 17:59:13 +1000

    Microbio stats and figs

--- elise_R_test1.R
+++ elise_R_test1.R
@@ -31,7 +31,7 @@ for (file in quizcat_files){
   i = i + 1
 }
 g=grid.arrange(grobs = quizcat_plots_test1[1:length(quizcat_files)])
-ggsave(file='quizcat_totals.pdf', g, width=15, height=10, units="in")
+ggsave(file='quizcat_totals_before.pdf', g, width=15, height=10, units="in")
 
 ### Loop through files and perform statistics
 quizcat_stats_test1 = list()
@@ -40,7 +40,6 @@ for (file in quizcat_files){
   quizData = read.csv(file)
   fit = kruskal.test(count_category ~ quiz_response, data=quizData)
   quizcat_stats_test1[[i]] = fit
-  #capture.output(as.data.frame(fit[1:5]), file = "quizcat_totals_stats.txt")
   i = i + 1
 }
 i = 1
@@ -55,4 +54,51 @@ for (stat in quizcat_stats_test1) {
   }
   i = i + 1
 }
-write.csv(mainDF, file = "quizcat_totals_stats.csv")
+write.csv(mainDF, file = "quizcat_totals_before_stats.csv")
+
+### Find files
+currdir = list.files()
+i = 1
+quizcat_files = list()
+for (val in currdir){
+  if(grepl('quizcat_totals_after', val)){
+    quizcat_files[[i]] = val
+    i = i + 1
+  }
+}
+
+### Loop through files and generate bar plots
+quizcat_plots_test1 = list()
+i = 1
+for (file in quizcat_files){
+  quizData = read.csv(file)
+  quizData$count_category <- factor(quizData$count_category, levels = c("0", "1-10", "11-100", "100-340", "TNTC"))
+  plot = ggplot(quizData) + geom_bar(aes(x = factor(quiz_response), fill = factor(count_category)), position = "fill") + xlab("Quiz response number") + ylab("Proportion of answers") + ggtitle(paste("Quiz question #", toString(i+1))) + guides(fill=guide_legend(title="Size category"))
+  quizcat_plots_test1[[i]] = plot
+  i = i + 1
+}
+g=grid.arrange(grobs = quizcat_plots_test1[1:length(quizcat_files)])
+ggsave(file='quizcat_totals_after.pdf', g, width=15, height=10, units="in")
+
+### Loop through files and perform statistics
+quizcat_stats_test1 = list()
+i = 1
+for (file in quizcat_files){
+  quizData = read.csv(file)
+  fit = kruskal.test(count_category ~ quiz_response, data=quizData)
+  quizcat_stats_test1[[i]] = fit
+  i = i + 1
+}
+i = 1
+for (stat in quizcat_stats_test1) {
+  tmpTable = as.data.frame(stat[1:5])
+  tmpTable$quiz_question = toString(i+1)
+  if (i == 1) {
+    mainDF = tmpTable
+  }
+  if (i != 1) {
+    mainDF <- rbind(tmpTable[1, ], mainDF)
+  }
+  i = i + 1
+}
+write.csv(mainDF, file = "quizcat_totals_after_stats.csv")
