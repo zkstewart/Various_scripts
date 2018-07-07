c39166b3690f78faef8fa74071c0a63c81973378
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2018-07-07 14:05:45 +1000

    R Scripts for micro analysis

--- elise_R_test1.R
+++ elise_R_test1.R
@@ -5,7 +5,11 @@ library(ggplot2)
 library(gridExtra)
 
 # Read data
-setwd("E:/elise/R_scripts")
+maindir = "E:/elise/R_scripts"
+setwd(maindir)
+
+# Specify output dir
+outdir = paste(maindir, "/outputs", sep="")
 
 # Perform statistics
 ## Test 1: growth before / growth after for quiz responses
@@ -14,7 +18,12 @@ currdir = list.files()
 i = 1
 quizcat_files = list()
 for (val in currdir){
-  if(grepl('quizcat_totals_before', val)){
+  if(grepl('.pdf', val)){
+    donothing=1
+  } else if (grepl('_stats', val)){
+    donothing=1
+  }
+  else if (grepl('quizcat_totals_before', val)){
     quizcat_files[[i]] = val
     i = i + 1
   }
@@ -25,13 +34,15 @@ quizcat_plots_test1 = list()
 i = 1
 for (file in quizcat_files){
   quizData = read.csv(file)
-  quizData$count_category <- factor(quizData$count_category, levels = c("0", "1-10", "11-100", "100-340", "TNTC"))
+  #quizData$count_category <- factor(quizData$count_category, levels = c("0", "1-10", "11-100", "100-340", "TNTC"))
+  #quizData$count_category <- factor(quizData$count_category, levels = c(1, 2, 3, 4, 5))
+  quizData$count_category <- factor(quizData$count_category, levels = c("1", "2", "3", "4", "5"))
   plot = ggplot(quizData) + geom_bar(aes(x = factor(quiz_response), fill = factor(count_category)), position = "fill") + xlab("Quiz response number") + ylab("Proportion of answers") + ggtitle(paste("Quiz question #", toString(i+1))) + guides(fill=guide_legend(title="Size category"))
   quizcat_plots_test1[[i]] = plot
   i = i + 1
 }
 g=grid.arrange(grobs = quizcat_plots_test1[1:length(quizcat_files)])
-ggsave(file='quizcat_totals_before.pdf', g, width=15, height=10, units="in")
+ggsave(file=paste(outdir,"/", 'TEST1_quizcat_totals_before.pdf', sep=""), g, width=15, height=10, units="in")
 
 ### Loop through files and perform statistics
 quizcat_stats_test1 = list()
@@ -54,14 +65,19 @@ for (stat in quizcat_stats_test1) {
   }
   i = i + 1
 }
-write.csv(mainDF, file = "quizcat_totals_before_stats.csv")
+write.csv(mainDF, file = paste(outdir,"/", "TEST1_quizcat_totals_before_stats.csv", sep=""))
 
 ### Find files
 currdir = list.files()
 i = 1
 quizcat_files = list()
 for (val in currdir){
-  if(grepl('quizcat_totals_after', val)){
+  if(grepl('.pdf', val)){
+    donothing=1
+  } else if (grepl('_stats', val)){
+    donothing=1
+  }
+  else if (grepl('quizcat_totals_after', val)){
     quizcat_files[[i]] = val
     i = i + 1
   }
@@ -72,13 +88,14 @@ quizcat_plots_test1 = list()
 i = 1
 for (file in quizcat_files){
   quizData = read.csv(file)
-  quizData$count_category <- factor(quizData$count_category, levels = c("0", "1-10", "11-100", "100-340", "TNTC"))
+  #quizData$count_category <- factor(quizData$count_category, levels = c("0", "1-10", "11-100", "100-340", "TNTC"))
+  quizData$count_category <- factor(quizData$count_category, levels = c("1", "2", "3", "4", "5"))
   plot = ggplot(quizData) + geom_bar(aes(x = factor(quiz_response), fill = factor(count_category)), position = "fill") + xlab("Quiz response number") + ylab("Proportion of answers") + ggtitle(paste("Quiz question #", toString(i+1))) + guides(fill=guide_legend(title="Size category"))
   quizcat_plots_test1[[i]] = plot
   i = i + 1
 }
 g=grid.arrange(grobs = quizcat_plots_test1[1:length(quizcat_files)])
-ggsave(file='quizcat_totals_after.pdf', g, width=15, height=10, units="in")
+ggsave(file=paste(outdir,"/", 'TEST1_quizcat_totals_after.pdf',sep=""), g, width=15, height=10, units="in")
 
 ### Loop through files and perform statistics
 quizcat_stats_test1 = list()
@@ -101,4 +118,4 @@ for (stat in quizcat_stats_test1) {
   }
   i = i + 1
 }
-write.csv(mainDF, file = "quizcat_totals_after_stats.csv")
+write.csv(mainDF, file = paste(outdir,"/", "TEST1_quizcat_totals_after_stats.csv", sep=""))
