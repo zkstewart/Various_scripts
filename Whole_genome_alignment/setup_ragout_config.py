acaed9276e5634d6fad0160180a11bd8ae095630
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2021-07-14 16:57:19 +1000

    setup_ragout_config allow for specification of quality type directly

--- Whole_genome_alignment/setup_ragout_config.py
+++ Whole_genome_alignment/setup_ragout_config.py
@@ -14,36 +14,43 @@ def validate_args(args):
             print(key + ' argument was not specified. Fix this and try again.')
             quit()
     # Validate input file locations
-    #fullPathReferences = []
-    for refFile in args.references:
+    for refFile in args.draftRefs:
         if not os.path.isfile(refFile):
             print('I am unable to locate the reference genome file (' + refFile + ')')
             print('Make sure you\'ve typed the file name or location correctly and try again.')
             quit()
-        #else:
-        #    fullPathReferences.append(os.path.abspath(refFile))
+    for refFile in args.completeRefs:
+        if not os.path.isfile(refFile):
+            print('I am unable to locate the reference genome file (' + refFile + ')')
+            print('Make sure you\'ve typed the file name or location correctly and try again.')
+            quit()
     if not os.path.isfile(args.target):
         print('I am unable to locate the target genome file (' + args.target + ')')
         print('Make sure you\'ve typed the file name or location correctly and try again.')
         quit()
-    #else:
-    #    fullPathTarget = os.path.abspath(args.target)
     # Validate output file location
     if os.path.isfile(args.outputFileName):
         print('File already exists at output location (' + args.outputFileName + ')')
         print('Make sure you specify a unique file name and try again.')
         quit()
-    #return fullPathReferences, fullPathTarget
 
-def format_ragout_config(referenceGenomeFiles, targetGenomeFile, outputConfigName, isDraft):
+def format_ragout_config(draftReferenceFiles, completeReferenceFiles, targetGenomeFile, outputConfigName):
     # Format reference values
-    refNames = []
-    refPaths = []
-    for refFile in referenceGenomeFiles:
+    draftRefNames = []
+    draftRefPaths = []
+    for refFile in draftReferenceFiles:
         refName = os.path.basename(refFile.rsplit(".", maxsplit=1)[0]).lower()
         path = "{0}.fasta = {1}".format(refName, refFile)
-        refNames.append(refName)
-        refPaths.append(path)
+        draftRefNames.append(refName)
+        draftRefPaths.append(path)
+    
+    completeRefNames = []
+    completeRefPaths = []
+    for refFile in completeReferenceFiles:
+        refName = os.path.basename(refFile.rsplit(".", maxsplit=1)[0]).lower()
+        path = "{0}.fasta = {1}".format(refName, refFile)
+        completeRefNames.append(refName)
+        completeRefPaths.append(path)
 
     # Format target values
     targetName = os.path.basename(targetGenomeFile.rsplit(".", maxsplit=1)[0]).lower()
@@ -51,17 +58,26 @@ def format_ragout_config(referenceGenomeFiles, targetGenomeFile, outputConfigNam
 
     # Write to file
     with open(outputConfigName, "w") as fileOut:
+        # Specify the names for genomes
         fileOut.write("#reference and target genome names (required)\n")
-        fileOut.write(".references = {0}\n".format(",".join(refNames)))
+        fileOut.write(".references = {0}\n".format(",".join(draftRefNames + completeRefNames)))
         fileOut.write(".target = {0}\n\n".format(targetName)) # Leave a blank line after
 
+        # Format the draft names, specifying that they are draft quality
         fileOut.write("#paths to genome fasta files (required for Sibelia)\n")
-        for path in refPaths:
+        for i in range(len(draftRefPaths)):
+            path = draftRefPaths[i]
+            name = draftRefNames[i]
             fileOut.write("{0}\n".format(path))
+            fileOut.write("{0}.draft = true\n".format(name))
+        
+        # Format the complete names, leaving quality default to indicate they are complete
+        for i in range(len(completeRefPaths)):
+            path = completeRefPaths[i]
+            fileOut.write("{0}\n".format(path))
+        
+        # Add the target name
         fileOut.write("{0}\n".format(targetPath))
-
-        if isDraft:
-            fileOut.write("\n*.fasta = true\n") # Specify that all genomes are in draft form, with separation between it and prior details
 
 def main():
     # User input
@@ -70,19 +86,19 @@ def main():
     for use by Ragout to scaffold the target genome. It does not run Ragout itself.
     """
     p = argparse.ArgumentParser(description=usage)
-    p.add_argument("-r", dest="references", nargs="+",
-        help="Input reference genome files")
+    p.add_argument("-rd", dest="draftRefs", nargs="+",
+        help="Input reference genome files which are DRAFT quality")
+    p.add_argument("-rc", dest="completeRefs", nargs="+",
+        help="Input reference genome files which are COMPLETE/CHROMOSOME quality")
     p.add_argument("-t", dest="target",
         help="Input target genome file")
     p.add_argument("-o", dest="outputFileName",
         help="Output file name for the config file")
-    p.add_argument("-d", dest="drafts", action="store_true", default=False,
-        help="Optionally specify that the references are drafts (unspecified == not draft)")
     args = p.parse_args()
     validate_args(args)
 
     # Generate config file
-    format_ragout_config(args.references, args.target, args.outputFileName, args.drafts)
+    format_ragout_config(args.draftRefs, args.completeRefs, args.target, args.outputFileName)
 
 if __name__ == "__main__":
     main()
