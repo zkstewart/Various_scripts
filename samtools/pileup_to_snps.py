b3bfaad00f31e337327318106596e5892ed45a9d
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2022-02-03 14:44:37 +1000

    pileup_to_snps more work filtering mpileup output
    I need to use samtools / bcftools to call alleles since I can't wrangle the maths myself

--- samtools/pileup_to_snps.py
+++ samtools/pileup_to_snps.py
@@ -3,7 +3,7 @@
 # Script to parse a samtools pileup produced by mpileup and
 # identify SNPs using custom filtering rules
 
-import os, argparse
+import os, argparse, copy, re
 import matplotlib.pyplot as plt
 import numpy as np
 import pickle
@@ -27,11 +27,11 @@ def validate_args(args):
     if args.coverageCutoff < 0:
         print("coverageCutoff should be greater than or equal to 0")
         quit()
-    if args.maf <= 0.00:
-        print("maf should be greater than 0")
+    if args.mafCutoff <= 0.00:
+        print("mafCutoff should be greater than 0")
         quit()
-    if args.maf >= 1.00:
-        print("maf should be less than 1")
+    if args.mafCutoff >= 1.00:
+        print("mafCutoff should be less than 1")
         quit()
 
 ## Data filtering
@@ -46,22 +46,118 @@ def mpileup_to_snpPiles(mpileupFile):
             ref = l[2]
             piles = [l[i:i+3] for i in range(3, len(l), 3)]
             # Store results
-            snpPiles.append([chrom, pos, ref, piles])
+            snpPiles.append([chrom, pos, ref, piles]) # This forms a pileGroup
     return snpPiles
 
-def filter_snpPiles(snpPiles, floorCount, coverageCutoff):
+def filter_snpPiles(snpPiles, floorCount, coverageCutoff, mafCutoff):
     filteredPiles = []
-    for pile in snpPiles:
+    for pileGroup in snpPiles:
         # Filter 1: Floor count
-        if int(pile[0]) < floorCount:
+        if int(pileGroup[0]) < floorCount:
             continue
         # Filter 2: Total coverage
-        totalCoverage = sum([int(p[0]) for p in pile[3]]) # pile[3] is the list of triplets [coverage, description, ASCII score]
+        totalCoverage = sum([int(pile[0]) for pile in pileGroup[3]]) # pileGroup[3] is the list of triplets [coverage, description, ASCII score]
         if totalCoverage > coverageCutoff:
             continue
+        # Filter 3: MAF cutoff
+        alleleCount = []
+        for pile in pileGroup[3]:
+            alleleFreqs = allele_frequency_from_subpile(pileGroup[2], pile) # pileGroup[2] is the reference base
+            diploidAlleles = call_diploid_alleles_from_alleleFreqs(alleleFreqs)
+            
         # Store results if above filters pass
         filteredPiles.append(pile)
     return filteredPiles
+
+def allele_frequency_from_subpile(ref, pile):
+    '''
+    This function assumes it is receiving a triplet-formatted pile, where
+    the first value is the read count, the second value are the samtools read
+    bases encoding, and the third is the ASCII quality score for each read.
+    Refer to http://www.htslib.org/doc/samtools-mpileup.html for details on the
+    samtools pileup format.
+    
+    It also needs to know the reference base. Provide that please.
+    '''
+    newpile = copy.deepcopy(pile) # prevent changes to the original pile
+    bases = newpile[1]
+    
+    # Remove symbols irrelevant to our calculations
+    bases = re.sub("\^.", "", bases) # ^ is followed by the ASCII quality of the read alignment; we don't need it
+    bases = bases.replace(">", "").replace("<", "") # I don't know how to handle reference skips
+    
+    # Get the allele for each position
+    ## Handle insertions and deletions AFTER the read base
+    insertAfter = re.findall(r"\+[0-9]+[ACGTNacgtn*#]+", bases) # count...
+    bases = re.sub(r"\+[0-9]+[ACGTNacgtn*#]+", "", bases) # then remove them
+    deleteAfter = re.findall(r"-[0-9]+[ACGTNacgtn]+", bases) # count...
+    bases = re.sub(r"-[0-9]+[ACGTNacgtn]+", "", bases) # then remove them
+    
+    ## Handle simple matches, mismatches, and deletions
+    matches = re.findall(r"[\.,]", bases)
+    mismatches = re.findall(r"[ACGTNacgtn]", bases)
+    deletions = re.findall(r"[\*#]", bases)
+    
+    # Tally the alleles
+    alleles = {}
+    for entry in [insertAfter, deleteAfter]:
+        for value in entry:
+            if value not in alleles:
+                alleles[value] = 1
+            else:
+                alleles[value] += 1
+                
+    alleles[ref] = len(matches)
+    
+    for mm in mismatches:
+        mm = mm.upper()
+        if mm not in alleles:
+            alleles[mm] = 1
+        else:
+            alleles[mm] += 1
+    
+    if len(deletions) > 0:
+        alleles["deletion"] = len(deletions)
+    
+    # Calculate frequencies
+    total = sum(alleles.values())
+    alleleFreqs = {}
+    for key, value in alleles.items():
+        freq = value / total
+        alleleFreqs[key] = freq
+    
+    return alleleFreqs
+
+def call_diploid_alleles_from_alleleFreqs(alleleFreqs):
+    '''
+    alleleFreqs is assumed to be a dictionary with key:value pairs
+    corresponding to the putative allele, and its frequency
+    as a float ratio which should sum to approximately 1.0.
+    
+    A simple assumption is made in this function that all samples are
+    diploid. This means we're going to see a monoallelic or biallelic
+    position. If it's biallelic, our ratio should be close to 0.5 : 0.5
+    for the two variants, otherwise we should see something close to
+    1.0 : 0.0. Deviations outside of this range will be assumed to be noise.
+    '''
+    MONOALLELIC_CUTOFF = 0.7
+    BIALLELIC_CUTOFF = 0.3
+    
+    # Get an ordered list of frequencies from alleleFreqs
+    orderedFreqs = []
+    for key, value in alleleFreqs.items():
+        orderedFreqs.append([key, value])
+    orderedFreqs.sort(key = lambda x: -x[1])
+    
+    # Handle scenario 1: monoallelic
+    if orderedFreqs[0][1] >= MONOALLELIC_CUTOFF:
+        return [orderedFreqs[0][0], orderedFreqs[0][0]]
+    # Handle scenario 2: biallelic
+    if len(orderedFreqs) > 1 and orderedFreqs[1][1] >= BIALLELIC_CUTOFF:
+        return [orderedFreqs[0][0], orderedFreqs[1][0]]
+    # Handle uncertainty (default to null hypothesis - there is no SNP here.)
+    else:
+        return [orderedFreqs[0][0], orderedFreqs[0][0]]
 
 ## Data exploration
 def plot_pile_statistics(snpPiles, boxplotName, histogramName):
@@ -109,7 +205,7 @@ def main():
         help="""This number is the maximum cumulative read coverage
         allowed for a site before being discarded as a repeat region; default=0""",
         default=0)
-    p.add_argument("--maf", dest="maf", type=float,
+    p.add_argument("--mafCutoff", dest="mafCutoff", type=float,
         help="""This number is the minor allele frequency (MAF) required
         for a SNP to be called; default=0.04""",
         default=0.04)
