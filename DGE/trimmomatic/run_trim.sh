7c9bcec39fef40b47bf32cfb7cd80c230944983c
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2022-02-22 12:11:45 +1000

    run_trim.sh more general purpose script

--- DGE/trimmomatic/run_trim.sh
+++ DGE/trimmomatic/run_trim.sh
@@ -1,42 +1,55 @@
 #!/bin/bash -l
-#PBS -N trim_alm
-#PBS -l walltime=80:00:00
-#PBS -l mem=50G
-#PBS -l ncpus=1
-#PBS -J 1-69
+#PBS -N trim_kirra
+#PBS -l walltime=24:00:00
+#PBS -l mem=25G
+#PBS -l ncpus=2
+#PBS -J 1-4
 
-cd d:\Libraries\Documents\GitHub\Various_scripts\DGE\trimmomatic
+cd $PBS_O_WORKDIR
 
 ### MANUAL SETUP BELOW
 ## SETUP: Load modules
 module load java/1.8.0_92
 
 ## SETUP: Specify trimmomatic location
-TRIMDIR=D:\Bioinformatics\Protein_analysis\Trimmomatic-0.36
+TRIMDIR=/home/stewarz2/various_programs/Trimmomatic-0.36
 TRIMJAR=trimmomatic-0.36.jar
 
 ## SETUP: Specify file prefixes
-SPECIES=alm
+SPECIES=btk
 
-## SETUP: Specify RNAseq files
-RNAFILES="d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/1_18-12-2019_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/2_18-12-2019_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/3_18-12-2019_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/5_18-12-2019_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/6_18-12-2019_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/7_18-12-2019_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/9_02-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/10_02-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/11_02-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/13_02-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/14_02-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/15_02-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/17_16-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/18_16-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/19_16-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/20_16-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/21_16-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/22_16-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/23_16-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/24_16-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/25_30-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/26_30-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/27_30-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/28_30-01-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/29_30-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/30_30-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/31_30-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/32_30-01-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/33_13-02-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/34_13-02-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/35_13-02-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/37_13-02-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/38_13-02-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/39_13-02-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/41_12-03-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/42_12-03-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/43_12-03-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/44_12-03-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/45_12-03-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/46_12-03-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/47_12-03-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/48_12-03-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/49_26-03-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/50_26-03-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/51_26-03-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/52_26-03-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/53_26-03-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/54_26-03-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/55_26-03-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/56_26-03-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/57_09-04-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/58_09-04-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/59_09-04-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/60_09-04-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/61_09-04-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/62_09-04-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/63_09-04-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/64_09-04-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/65_23-04-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/66_23-04-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/67_23-04-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/68_23-04-2020_Leaf d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/69_23-04-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/70_23-04-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/71_23-04-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/73_07-05-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/74_07-05-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/75_07-05-2020_Bud d:/Libraries/Documents/GitHub/Various_scripts/DGE/rnaseq_reads/76_07-05-2020_Bud"
-ARRAY=($RNAFILES)
-# Note: FILEPREFIXES assumes a file name like ${PREFIX}_1.fastq with paired ${PREFIX}_2.fastq
+## SETUP: Specify RNAseq reads dir
+READSDIR=/home/stewarz2/flies/kirra/reads/concat
+SUFFIX=.fastq.gz
+
+## SETUP: Specify computational parameters
+CPUS=2
+# SETUP END
 ### MANUAL SETUP END
 
-### AUTOMATIC SETUP START
-## SETUP: Paired-end trimmomatic settings
-COMMAND="ILLUMINACLIP:D:\Bioinformatics\Protein_analysis\Trimmomatic-0.36/adapters/TruSeq3-PE.fa:2:30:10 SLIDINGWINDOW:4:5 LEADING:5 TRAILING:5 MINLEN:25"
-### AUTOMATIC SETUP END
 
-### RUN PROGRAM
+COMMAND="ILLUMINACLIP:/home/stewarz2/various_programs/Trimmomatic-0.36/adapters/TruSeq3-PE.fa:2:30:10 SLIDINGWINDOW:4:5 LEADING:5 TRAILING:5 MINLEN:25"
+
+
+#####
+
+# RUN START
+## STEP 1: Find RNAseq file prefixes
+declare -a RNAFILES
+i=0
+for f in ${READSDIR}/*/*1${SUFFIX}; do
+    RNAFILES[${i}]=$(echo "${f%%_R1${SUFFIX}}");
+    i=$((i+1));
+done
+
+## STEP 2: Get job details
 ARRAY_INDEX=$((${PBS_ARRAY_INDEX}-1))
-FILE=${ARRAY[${ARRAY_INDEX}]}
-BASENAME=$(basename ${FILE})
+FILEPREFIX=${RNAFILES[${ARRAY_INDEX}]}
+BASEPREFIX=$(basename ${FILEPREFIX})
 
 ## STEP 1: Run Trimmomatic
-java -jar $TRIMDIR/$TRIMJAR PE -threads 1 -trimlog $SPECIES.logfile ${FILE}_R1.fastq.gz ${FILE}_R2.fastq.gz -baseout d:\Libraries\Documents\GitHub\Various_scripts\DGE\trimmomatic/${BASENAME}.trimmed.fq.gz ${COMMAND}; done
+java -jar $TRIMDIR/$TRIMJAR PE -threads ${CPUS} -trimlog ${SPECIES}.logfile ${FILEPREFIX}_R1${SUFFIX} ${FILE}_R2${SUFFIX} -baseout ${BASEPREFIX}.trimmed.fq.gz ${COMMAND}
 
 ## STEP 2: Unzip files
-gunzip d:\Libraries\Documents\GitHub\Various_scripts\DGE\trimmomatic/${BASENAME}.trimmed_1P.fq.gz d:\Libraries\Documents\GitHub\Various_scripts\DGE\trimmomatic/${BASENAME}.trimmed_2P.fq; done
+gunzip ${BASEPREFIX}.trimmed_1P.fq.gz ${BASEPREFIX}.trimmed_2P.fq
     
