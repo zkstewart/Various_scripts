286c51d3ad8e42b412e1a8843e2a5d544cb4c0f5
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2022-03-02 08:47:33 +1000

    Create exome_curation_prep.py
    Init of script that will eventually be used in project work

---
+++ exome_project/exome_curation_prep.py
@@ -0,0 +1,12 @@
+#! python3
+# exome_curation_prep.py
+# Program to enable manual curation of exome sequencing
+# to occur for the Oz Mammals Genomics initiative as part
+# of Matthew Phillips and Andrew Baker (et. al.'s) group.
+
+import sys
+sys.path.append('../')
+from Function_packages import ZS_SeqIO
+
+if __name__ == "__main__":
+    pass
