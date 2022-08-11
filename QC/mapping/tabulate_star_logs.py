2f4d310e13b52f9e02241a678fef9d888c5b6704
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2022-08-11 13:43:44 +1000

    Create tabule_star_logs and fix merge_star_stats

--- QC/mapping/tabulate_star_logs.py
+++ QC/mapping/tabulate_star_logs.py
@@ -1,7 +1,8 @@
 #! python3
-# merge_star_stats.py
-# A simple python program to combine STAR output ReadsPerGene.out.tab
-# files into a table that's amenable to my way of doing DGE analysis
+# tabulate_star_logs.py
+# Simple script to read in the Log.final.out files
+# produced by STAR after mapping multiple samples and
+# tabulates these into a format that's easy to read.
 
 import os, argparse
 
@@ -18,56 +19,57 @@ def validate_args(args):
         print('Make sure you specify a unique file name and try again.')
         quit()
 
-def get_counts_from_files(countsFiles):
+def get_mapping_stats_from_files(logsFiles):
     '''
     Parameters:
-        countsFiles -- a list containing strings that point to STAR output files
-                       containing reads counts (ReadsPerGene.out.tab)
+        logsFiles -- a list containing strings that point to STAR output files
+                     containing read mapping statistics (Log.final.out)
     Returns:
         statsTable -- a list of lists with structure like:
                       [
                           [header_row_values (\t separated sample names)],
-                          [gene_id_1\tsample_1_count\tsample_2_count...],
+                          [statistic_type_1\tsample_1_result\tsample_2_result...],
                           ...
                       ]
     '''
-    IRRELEVANT_LINES = [
-        "N_unmapped", "N_multimapping",
-        "N_noFeature", "N_ambiguous"
+    RELEVANT_LINES = [
+        r"Uniquely mapped reads %", r"% of reads mapped to multiple loci",
+        r"% of reads mapped to too many loci", r"% of reads unmapped: too many mismatches",
+        r"% of reads unmapped: too short", r"% of chimeric reads"
     ]
-    fileCount = 0
-    statsTable=["gene_id"]
     
-    for file in countsFiles:
+    # Setup the output table format
+    statsTable=[""]
+    for rl in RELEVANT_LINES:
+        statsTable.append(rl)
+    
+    # Parse all logs files
+    for file in logsFiles:
         # Get the sample name from the directory path
         "Since we have the absolute path, we assume the parent directory is the sample name"
         sampleName = os.path.basename(os.path.dirname(file))
         
         # Parse the file
-        lineCount = 0
         with open(file, "r") as fileIn:
             statsTable[0] += "\t{0}".format(sampleName) # on first line, add to header row
             
             for line in fileIn:
-                sl = line.rstrip("\r\n").split("\t")
-                if sl == []:
-                    continue
-                geneName, count, _, _ = sl # drop last 2 stranded counts, just work with unstranded count
-                
+                l = line.strip("\r\n ")
                 # Skip irrelevant lines
-                if geneName in IRRELEVANT_LINES:
+                isRelevant = any([True if l.startswith(rl) else False for rl in RELEVANT_LINES])
+                if not isRelevant:
                     continue
                 
-                # First file being checked
-                if fileCount == 0:
-                    statsTable.append("\t".join([geneName, count]))
-                    
-                # All other files
-                else:
-                    statsTable[lineCount + 1] += "\t{0}".format(count)
+                # Extract information from line
+                statType, stat = l.split("|")
+                statType = statType.strip("\t ")
+                stat = stat.strip("\t ")
                 
-                lineCount += 1
-        fileCount += 1
+                # Get the index for this stat line
+                tableIndex = RELEVANT_LINES.index(statType) + 1 # add one since statsTable starts with header row
+                
+                # All other files
+                statsTable[tableIndex] += "\t{0}".format(stat)
     
     return statsTable
 
@@ -75,8 +77,8 @@ def get_counts_from_files(countsFiles):
 def main():
     # User input
     usage = """%(prog)s accepts a directory containing subdirectories that house
-    STAR output files, notably the ReadsPerGene.out.tab file, and writes a condensed
-    table of read counts to the specified output file.
+    STAR output files, notably the Log.final.out file, and writes a condensed
+    table of read mapping percentages to the output file.
     """
     p = argparse.ArgumentParser(description=usage)
     ## Required
@@ -88,18 +90,18 @@ def main():
     args = p.parse_args()
     validate_args(args)
     
-    # Get ReadsPerGene.out.tab files list
-    countsFiles = []
+    # Get Log.final.out files list
+    logsFiles = []
     for fileOrDir in os.listdir(args.starOutputsDir):
         if os.path.isdir(os.path.join(args.starOutputsDir, fileOrDir)):
             for subFileOrDir in os.listdir(os.path.join(args.starOutputsDir, fileOrDir)):
-                if subFileOrDir == "ReadsPerGene.out.tab":
-                    countsFiles.append(os.path.join(
+                if subFileOrDir == "Log.final.out":
+                    logsFiles.append(os.path.join(
                         args.starOutputsDir, fileOrDir, subFileOrDir
                     ))
     
     # Combine ReadsPerGene.out.tab files
-    statsTable = get_counts_from_files(countsFiles)
+    statsTable = get_mapping_stats_from_files(logsFiles)
     
     # Write output
     with open(args.outputFileName, "w") as fileOut:
