54f92670092f0a6d71dee5f15058883f59b30824
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2023-05-12 17:11:07 +1000

    run_fastqc improvements

--- QC/reads/run_fastqc.sh
+++ QC/reads/run_fastqc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-#PBS -N fqc
-#PBS -l walltime=24:00:00
+#PBS -N rawfqc
+#PBS -l walltime=02:00:00
 #PBS -l mem=25G
 #PBS -l ncpus=4
-#PBS -J 1-12
+#PBS -J 1-16
 
 cd $PBS_O_WORKDIR
 
@@ -12,8 +12,9 @@ cd $PBS_O_WORKDIR
 module load fastqc/0.11.7-java-1.8.0_92
 
 ## SETUP: Specify RNAseq reads dir
-READSDIR=/home/stewarz2/micro_group/carrie/reads
-SUFFIX=.fastq.gz
+READSDIR=/home/stewarz2/banana_group/metabolome/reads
+R1SUFFIX=_1.fq.gz
+R2SUFFIX=_2.fq.gz
 
 ## SETUP: Specify computational parameters
 CPUS=4
@@ -28,16 +29,16 @@ CPUS=4
 ## STEP 1: Find RNAseq file prefixes
 declare -a RNAFILES
 i=0
-for f in ${READSDIR}/*1${SUFFIX}; do
-    RNAFILES[${i}]=$(echo "${f%%1${SUFFIX}}");
+for f in ${READSDIR}/*${R1SUFFIX}; do
+    RNAFILES[${i}]=$(echo "${f%%${R1SUFFIX}}");
     i=$((i+1));
 done
 
 ## STEP 2: Get job details
 ARRAY_INDEX=$((${PBS_ARRAY_INDEX}-1))
 FILEPREFIX=${RNAFILES[${ARRAY_INDEX}]}
-BASEPREFIX=$(basename ${FILEPREFIX} _) # strip any _ trailing characters
+BASEPREFIX=$(basename ${FILEPREFIX})
 
 ## STEP 3: Run FASTQC
 mkdir -p ${BASEPREFIX}
-fastqc -o ${BASEPREFIX} -t ${CPUS} ${FILEPREFIX}1${SUFFIX} ${FILEPREFIX}2${SUFFIX}
+fastqc -o ${BASEPREFIX} -t ${CPUS} ${FILEPREFIX}${R1SUFFIX} ${FILEPREFIX}${R2SUFFIX}
