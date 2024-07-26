3b944ae0cf6a809ea1151b57567c8d44894e62ef
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2024-07-26 12:46:16 +1000

    msa_variant_report input overhaul; reportFormat opt

--- Fasta_related/Alignment/msa_variant_report.py
+++ Fasta_related/Alignment/msa_variant_report.py
@@ -10,31 +10,100 @@ from Function_packages import ZS_SeqIO, ZS_AlignIO
 
 def validate_args(args):
     # Validate input data location
-    if not os.path.isdir(args.msaDir):
-        print('I am unable to locate the directory where the MSA FASTA files are (' + args.msaDir + ')')
-        print('Make sure you\'ve typed the file name or location correctly and try again.')
-        quit()
+    args.files = []
+    for location in args.msas:
+        if os.path.isfile(location):
+            args.files.append(location)
+        elif os.path.isdir(location):
+            args.files.extend([
+                os.path.join(location, x)
+                for x in os.listdir(location)
+                if x.lower().endswith(tuple(args.suffixes))
+            ])
+        else:
+            raise FileNotFoundError((f"I am unable to locate the MSA file or directory at '{location}'. " + 
+                                    "Make sure you've typed the name or location correctly and try again."))
     # Validate logic of argument combination
     if args.asCodons and not args.isNucleotide:
-        print("You've specified --asCodons without --isNucleotide; provide them together or not at all")
-        quit()
-    if args.translationTable != 1 and not args.asCodons:
-        print("You've specified --translationTable without --asCodons; provide them together or not at all")
-        quit()
-    # Validate numeric arguments
-    # if args.translationTable < 1 or args.translationTable > 31:
-    #     print('The translation table must be an integer between 1 and 31')
-    #     quit()
+        raise ValueError("You've specified --asCodons without --isNucleotide; provide them together or not at all")
     # Handle file output
-    if os.path.isfile(args.outputFileName):
-        print(f'File already exists at output location ({args.outputFileName})')
-        print('Make sure you specify a unique file name and try again.')
-        quit()
+    if args.reportFormat == "per_variant":
+        if os.path.exists(args.outputLocation):
+            raise FileExistsError((f"File already exists at output location ({args.outputLocation}). " + 
+                                   "Make sure you specify a unique file name and try again."))
+        elif not os.path.isdir(os.path.dirname(os.path.abspath(args.outputLocation))):
+            FileNotFoundError((f"Output file '{args.outputLocation}' would be written to a non-existent directory" + 
+                               "If you provide a full path, make sure its parent directories exist; " +
+                               "otherwise, provide a file name only."))
+    elif args.reportFormat == "per_sequence":
+        if not os.path.isdir(os.path.dirname(os.path.abspath(args.outputLocation))):
+            FileNotFoundError((f"Output file '{args.outputLocation}' would be written to a non-existent directory" + 
+                               "If you provide a full path, make sure its parent directories exist; " +
+                               "otherwise, provide a directory name only."))
+        if os.path.isdir(args.outputLocation):
+            print((f"# Output location '{args.outputLocation}' already exists. Be warned that if you're trying to " +
+                   "resume a previous run, you should have deleted any flawed or incomplete files before " + 
+                   "proceeding since files will NOT be overwritten."))
+            print("# msa_variant_report.py execution will continue...")
+        else:
+            try:
+                os.mkdir(args.outputLocation)
+                print(f"# Output directory '{args.outputLocation}' created as part of argument validation.")
+            except:
+                raise FileNotFoundError((f"Unable to create output directory '{args.outputLocation}' for " + 
+                                         "unknown reasons. Please check the path and try again."))
+
+def report_per_variant(outputFileName, variantDict):
+    # Write report file
+    with open(outputFileName, "w") as fileOut:
+        # Write header
+        fileOut.write("#gene\tposition_number\tconsensus_residue\tvariant_residue\tseqs_with_variant\n")
+        
+        # Write content lines
+        for fileName, pairedValue in variantDict.items():
+            geneID = fileName.rsplit(".", maxsplit=1)[0]
+            positionDict = pairedValue[0] # pairedValue = [positionDict, fullFileName]
+            for position, detailsDict in positionDict.items():
+                for variantResidue, seqList in detailsDict["variants"].items():
+                    fileOut.write(f"{geneID}\t{position+1}\t{detailsDict['consensus']}\t" +
+                                f"{variantResidue}\t{', '.join(seqList)}\n")
+
+def report_per_sequence(outputDirectory, variantDict):
+    for fileName, pairedValue in variantDict.items():
+        geneID = fileName.rsplit(".", maxsplit=1)[0]
+        positionDict, fullFileName = pairedValue
+        
+        # Establish dictionary for storing reformatted data
+        FASTA_obj = ZS_SeqIO.FASTA(fullFileName, isAligned=True)
+        reformattedDict = { x.id : [] for x in FASTA_obj.seqs }
+        
+        # Reformat variants for per-sequence reporting
+        for position, detailsDict in positionDict.items():
+            for variantResidue, seqList in detailsDict["variants"].items():
+                for seqID in seqList:
+                    reformattedDict[seqID].append(f"{position+1}:{detailsDict['consensus']}:{variantResidue}")
+        
+        # Derive report file name and skip if it exists
+        reportFileName = os.path.join(outputDirectory, f"{geneID}.report.tsv")
+        if os.path.exists(reportFileName):
+            print(f"# WARNING: File already exists at '{reportFileName}' and will not be overwritten; skipping...")
+            continue
+        
+        # Write report file
+        with open(reportFileName, "w") as fileOut:
+            # Write header
+            fileOut.write("#sequence_id\tvariants\n")
+            
+            # Write content lines
+            for seqID, variantList in reformattedDict.items():
+                variants = ", ".join(variantList) if variantList != [] else "."
+                fileOut.write(f"{seqID}\t{variants}\n")
 
 def main():
     #### USER INPUT SECTION
-    usage = """%(prog)s will receive one or more input MSA FASTA files located within a specified
-    directory and identify any variants present in the alignment. In this context, a variant is
+    usage = """%(prog)s will receive one or more input MSA FASTA files specified by the -i argument.
+    Inputs can be specified as individual files or as a directory containing multiple files.
+    This script will identify any variants present in these aligned files. In this context, a variant is
     a nucleotide or amino acid residue that deviates from the consensus sequence. An output
     TSV will be generated listing all variants found, along with their positions in the alignment.
     Note that input can be provided as nucleotide or amino acid sequences; if providing a nucleotide
@@ -43,13 +112,26 @@ def main():
     """
     p = argparse.ArgumentParser(description=usage)
     # Required
-    p.add_argument("-i", dest="msaDir",
+    p.add_argument("-i", dest="msas",
                    required=True,
-                   help="Specify the location of the directory containing one or more MSA FASTA files")
-    p.add_argument("-o", dest="outputFileName",
+                   nargs="+",
+                   help="Specify locations of MSA files and/or director(y/ies) containing MSA files")
+    p.add_argument("-f", dest="reportFormat",
                    required=True,
-                   help="Output report file name")
+                   choices=["per_variant", "per_sequence"],
+                   help="Specify the format of the output report file(s)")
+    p.add_argument("-o", dest="outputLocation",
+                   required=True,
+                   help="""Output file name (if -f == 'per_variant')
+                   or directory (if 'per_sequence')""")
     # Optional
+    p.add_argument("--suffixes", dest="suffixes",
+                   required=False,
+                   nargs="+",
+                   help="""Optionally indicate the suffixes of the MSA files you'd like to
+                   obtain from any directories specified in -i; default == 
+                   '.fasta .fas .fa .faa .fna .aa .cds .trans .nucl'""",
+                   default=[".fasta", ".fas", ".fa", ".faa", ".fna", ".aa", ".cds", ".trans", ".nucl"])
     p.add_argument("--isNucleotide", dest="isNucleotide",
                    required=False,
                    action="store_true",
@@ -62,51 +144,40 @@ def main():
                    help="""Optionally provide this argument if you're providing a nucleotide
                    file and would like to consider codons as the unit of comparison""",
                    default=False)
-    # p.add_argument("--translationTable", dest="translationTable",
-    #                require=False,
-    #                type=int,
-    #                help="""Optionally specify the NCBI numeric genetic code to utilise for
-    #                CDS translation if --isNucleotide and --asCodons; this should be an integer
-    #                from 1 to 31 (default == 1 i.e., Standard Code)""",
-    #                default=1)
+    p.add_argument("--reportUntilStop", dest="reportUntilStop",
+                   required=False,
+                   action="store_true",
+                   help="""Optionally provide this argument if you do not want to see variants
+                   reported for a sequence after a stop codon is encountered in that sequence
+                   (variants will be reported in other sequences if they don't encounter that
+                   stop codon)""",
+                   default=False)
     
     args = p.parse_args()
     validate_args(args)
     
-    # Locate all FASTA files
-    files = [os.path.join(args.msaDir, file) for file in os.listdir(args.msaDir)]
-    
     # Iterate through each file and store variants
     variantDict = {}
-    for file in files:
-        filePrefix = os.path.splitext(os.path.basename(file))[0]
-        
+    for file in args.files:
         # Load in the aligned FASTA file
         FASTA_obj = ZS_SeqIO.FASTA(file, isAligned=True)
         FASTA_obj.make_uppercase() # make sure comparison isn't case-sensitive
         
         # Locate variants in this MSA file
-        variantDict[filePrefix] = ZS_AlignIO.MSA.locate_variants_from_msa(
-            FASTA_obj, args.isNucleotide, args.asCodons
-        )
+        variantDict[os.path.basename(file)] = [ZS_AlignIO.MSA.locate_variants_from_msa(
+            FASTA_obj, args.isNucleotide, args.asCodons, reportUntilStop=args.reportUntilStop
+        ), file] # store file location for report_per_sequence
     
     # Exit if no variants found
     if all([len(variantDict[file]) == 0 for file in variantDict]):
-        print("No variants found in any of the provided MSA files; program will exit without writing output.")
-        quit()
+        raise ValueError("No variants found in any of the provided MSA files; program will exit without writing output.")
     
     # Otherwise, write output report
     else:
-        with open(args.outputFileName, "w") as fileOut:
-            # Write header
-            fileOut.write("#gene\tposition_number\tconsensus_residue\tvariant_residue\tseqs_with_variant\n")
-            
-            # Write content lines
-            for geneID, positionDict in variantDict.items():
-                for position, detailsDict in positionDict.items():
-                    for variantResidue, seqList in detailsDict["variants"].items():
-                        fileOut.write(f"{geneID}\t{position+1}\t{detailsDict['consensus']}\t" +
-                                      f"{variantResidue}\t{', '.join(seqList)}\n")
+        if args.reportFormat == "per_variant":
+            report_per_variant(args.outputLocation, variantDict)
+        elif args.reportFormat == "per_sequence":
+            report_per_sequence(args.outputLocation, variantDict)
     
     # Done!
     print('Program completed successfully!')
