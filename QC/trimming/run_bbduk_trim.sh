6ece05c5f0bcf59b9c0103b41462dc82190b1e7b
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2025-04-14 14:28:16 +1000

    Update run_bbduk_trim.sh
    Aesthetic changes and fix for java import on new HPC

--- QC/trimming/run_bbduk_trim.sh
+++ QC/trimming/run_bbduk_trim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#PBS -N trim_bac
+#PBS -N trim_reads
 #PBS -l walltime=06:00:00
 #PBS -l mem=35G
 #PBS -l ncpus=4
@@ -7,29 +7,24 @@
 
 cd $PBS_O_WORKDIR
 
-### MANUAL SETUP BELOW
-## SETUP: Load modules
-module load java/1.8.0_92
+module load Java/17.0.6
 
-## SETUP: Specify bbmap location
+####
+
+# Specify bbmap location
 BBDIR=/home/stewarz2/various_programs/bbmap
 
-## SETUP: Specify RNAseq reads dir
+# Specify reads dir
 READSDIR=/home/stewarz2/flies/mitch/prepared_reads
 R1SUFFIX=_1.fq.gz
 R2SUFFIX=_2.fq.gz
 
-## SETUP: Specify computational parameters
+#  Specify computational parameters
 CPUS=4
-# SETUP END
-### MANUAL SETUP END
 
+####
 
-#####
-
-
-# RUN START
-## STEP 1: Find RNAseq file prefixes
+# STEP 1: Find file prefixes
 declare -a RNAFILES
 i=0
 for f in ${READSDIR}/*${R1SUFFIX}; do
@@ -37,12 +32,12 @@ for f in ${READSDIR}/*${R1SUFFIX}; do
     i=$((i+1));
 done
 
-## STEP 2: Get job details
+# STEP 2: Get job details
 ARRAY_INDEX=$((${PBS_ARRAY_INDEX}-1))
 FILEPREFIX=${RNAFILES[${ARRAY_INDEX}]}
 BASEPREFIX=$(basename ${FILEPREFIX})
 
-## STEP 3: Run bbduk for trimming
+# STEP 3: Run bbduk for trimming
 ${BBDIR}/bbduk.sh -Xmx10g in1=${FILEPREFIX}${R1SUFFIX} in2=${FILEPREFIX}${R2SUFFIX} \
 	out1=${BASEPREFIX}.trimmed_1P.fq out2=${BASEPREFIX}.trimmed_2P.fq \
 	ref=${BBDIR}/resources/adapters.fa threads=${CPUS} \
