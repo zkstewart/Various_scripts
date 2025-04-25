6dcb209ee88c641e562542e04a52d9b34b12804c
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2025-04-25 17:24:25 +1000

    nanopore_map major update
    Brings the script into conformity with how illumina_map.py works and borrows functions from there where appropriate. More flexibility with how minimap2 is called now.

--- mapping/nanopore/nanopore_map.py
+++ mapping/nanopore/nanopore_map.py
@@ -2,139 +2,90 @@
 # Script to handle Nanopore single-end read mapping via minimap2
 # It generally follows the process of https://www.mdpi.com/1422-0067/21/23/9177/htm
 
-import os, argparse, distutils.spawn, gzip
+import os, sys, argparse, gzip, shutil
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from illumina.illumina_map import parse_metadata_csv, get_files_from_prefix, process_readgroups
 
 # Define functions
 def validate_args(args):
     # Validate input file location
     if not os.path.isfile(args.fastaFile):
-        print('I am unable to locate the genome FASTA file (' + args.fastaFile + ')')
-        print('Make sure you\'ve typed the file name or location correctly and try again.')
-        quit()
-    if not os.path.isfile(args.metadataCsv):
-        print('I am unable to locate the metadata CSV file (' + args.metadataCsv + ')')
-        print('Make sure you\'ve typed the file name or location correctly and try again.')
-        quit()
-    if not os.path.isdir(args.fastqDirectory):
-        print('I am unable to locate the FASTQ files directory (' + args.fastqDirectory + ')')
-        print('Make sure you\'ve typed the file name or location correctly and try again.')
-        quit()
-    # Validate executable locations
-    if not os.path.isfile(args.minimap2):
-        args.minimap2 = distutils.spawn.find_executable("minimap2")
-    if args.minimap2 == None or not os.path.isfile(args.minimap2):
-        if args.minimap2 == None:
-            print(f'I am unable to locate the minimap2 executable file')
-        else:
-            print(f'I am unable to locate the minimap2 executable file ({args.minimap2})')
-        print('Make sure you\'ve typed the file name or location correctly and try again.')
-        quit()
+        raise FileNotFoundError(f"I am unable to locate the genome FASTA file ({args.fastaFile})")
+    if not os.path.isfile(args.metadataFile):
+        raise FileNotFoundError(f"I am unable to locate the metadata file ({args.metadataFile})")
+    if not os.path.isdir(args.fastaqDirectory):
+        raise FileNotFoundError(f"I am unable to locate the FASTA/Q files directory ({args.fastaqDirectory})")
     
-    if not os.path.isfile(args.qcProgram):
-        args.qcProgram = distutils.spawn.find_executable(args.qcProgram)
-    if args.qcProgram == None or not os.path.isfile(args.qcProgram):
-        if args.minimap2 == None:
-            print('I am unable to locate the NanoFilt / Chopper executable file')
-        else:
-            print('I am unable to locate the NanoFilt / Chopper executable file (' + args.qcProgram + ')')
-        print('Make sure you\'ve typed the file name or location correctly and try again.')
-        quit()
+    # Validate program discoverability
+    if args.minimap2 == None:
+        args.minimap2 = shutil.which("minimap2")
+    if not os.path.isfile(args.minimap2):
+        raise FileNotFoundError(f"I am unable to locate the minimap2 executable file ({args.minimap2})")
+    
+    if args.runChopper: # only check if chopper is needed
+        if args.chopper == None:
+            args.chopper = shutil.which("chopper")
+        if not os.path.isfile(args.chopper):
+            raise FileNotFoundError(f"I am unable to locate the chopper executable file ({args.chopper})")
+    
+    # Validate numeric inputs
+    if args.cpus < 1:
+        raise ValueError(f"--cpus value '{args.cpus}' should be a positive integer (greater than zero)")
+    if args.minLength > 0 and not args.runChopper:
+        raise ValueError(f"Provide --minLength value only if --runChopper is set")
+    if args.maxLength > 0 and not args.runChopper:
+        raise ValueError(f"Provide --maxLength value only if --runChopper is set")
 
 def is_gz(file):
-    with gzip.open(file, 'r') as fh:
-        try:
-            fh.read(1)
-            return True
-        except OSError:
-            return False
+    if file.endswith(".gz"):
+        return True
+    else:
+        with gzip.open(file, "r") as fh:
+            try:
+                fh.read(1)
+                return True
+            except OSError:
+                return False
 
-def parse_metadata_csv(metadataCsv, speciesIdCol, genotypeCols):
-    header = None
-    speciesIds = []
-    genotypeIndices = []
-    genotypes = []
-    
-    with open(metadataCsv, "r") as fileIn:
-        for line in fileIn:
-            sl = line.rstrip("\r\n ").split(",")
-            # Handle header line
-            if header == None:
-                header = sl
-                # Raise errors if columns don't exist
-                if speciesIdCol not in header:
-                    raise Exception(f"Species ID column '{speciesIdCol}' does not exist in header '{header}'")
-                for gCol in genotypeCols:
-                    if gCol not in header:
-                        raise Exception(f"Genotype column '{gCol}' do not exist in header '{header}'")
-                    else:
-                        # Get column index if it exists
-                        genotypeIndices.append(header.index(gCol))
-                # Get column indices if they exist
-                speciesIdIndex = header.index(speciesIdCol)
-            # Handle content lines
-            else:
-                genotype = []
-                for gIndex in genotypeIndices:
-                    genotype.append(sl[gIndex])
-                genotype = "_".join(genotype)
-                
-                assert genotype not in genotypes, "Genotype '{0}' isn't unique!".format(genotype)
-                genotypes.append(genotype)
-                speciesIds.append(sl[speciesIdIndex])
-    
-    return speciesIds, genotypes
-
-def process_readgroups(speciesIds, genotypes, library, unit):
-    readgroups=[]
-    for i in range(len(genotypes)):
-        readgroups.append("@RG\\tID:{0}\\tSM:{1}\\tPL:{2}\\tLB:{3}\\tPU:{4}".format(speciesIds[i], genotypes[i], "nanopore", library, unit))
-    return readgroups
-
-def process_fqfiles(speciesIds, fqDir, suffix=".fq.gz"):
-    fqFiles=[]
-    for i in range(len(speciesIds)):
-        fqFileName = os.path.join(fqDir, "{0}{1}".format(speciesIds[i], suffix))
-        
-        if not os.path.isfile(fqFileName):
-            print(f"I expected to find the file {fqFileName}, but failed to do so!")
-            print("Either fix your metadata, or make sure the indicated file exists.")
-            quit()
-        
-        fqFiles.append(fqFileName)
-    return fqFiles
-
-def create_cmd_file(speciesIds, fqDir, readgroups, genomeFile, minimap2Exe,
-                    qcExe, minLength, maxLength, outputFileName, fqSuffix=".fq.gz"):
-    fqFiles = process_fqfiles(speciesIds, fqDir, fqSuffix)
-    
+def create_cmd_file(fastaqFiles, speciesIds, readgroups, genomeFile, minimap2Exe, preset, 
+                    chopperExe, runChopper, minLength, maxLength, outputFileName,
+                    readsSuffix=".fq.gz", cpus=1):    
     # Validations
     if os.path.isfile(outputFileName):
-        raise FileExistsError(f"File name '{outputFileName}' already exists!")
+        raise FileExistsError(f"'{outputFileName}' already exists; delete, move, or rename then try again")
+    assert len(fastaqFiles) == len(readgroups)
     
-    if len(fqFiles) != len(readgroups):
-        print(f"The number of samples indicated in your metadata {len(readgroups)} " +
-              f"differs to the number of FQ files found {len(fqFiles)}")
-        print("Make sure these values corroborate each other, then try again.")
-        quit()
-    
-    # Generate the cmd file
+    # Write to file
     with open(outputFileName, "w") as fileOut:
-        for i in range(len(fqFiles)):
-            # Get relevant details for this sample
+        for i in range(len(fastaqFiles)):
             sid = speciesIds[i]
             fq = fqFiles[i]
             rg = readgroups[i]
-            isGzip = is_gz(fq)
             
-            # Format a command and write to file
-            if isGzip:
-                cmd = f"gunzip -c {fq} | {qcExe} -l {minLength} --maxlength {maxLength} 2> {sid}.qc | "
+            # Format the cmd with any pre-processing QC steps
+            if runChopper:
+                if is_gz(fq):
+                    cmd = f"gunzip -c {fq} | {chopperExe}"
+                else:
+                    cmd = f"cat {fq} | {chopperExe}"
+                
+                if minLength > 0:
+                    cmd += f" -l {minLength}"
+                if maxLength > 0:
+                    cmd += f" --maxlength {maxLength}"
+                cmd += f"2> {sid}.qc | "
             else:
-                cmd = f"cat {fq} | {qcExe} -l {minLength} --maxlength {maxLength} 2> {sid}.qc | "
-            cmd += f"{minimap2Exe} -R '{rg}' -ax map-ont {genomeFile} - > {sid}.sam\n"
+                if is_gz(fq):
+                    cmd = f"gunzip -c {fq} | "
+                else:
+                    cmd = f"cat {fq} | "
+            
+            # Add and write the minimap2 command
+            cmd += f"{minimap2Exe} -t {cpus} -R '{rg}' -ax {preset} {genomeFile} - > {sid}.sam\n"
             fileOut.write(cmd)
 
-def create_shell_script(cmdFile, numJobs, outputFileName="run_nanopore_map.sh"):
+def create_shell_script(cmdFile, numJobs, outputFileName="run_nanopore_map.sh", cpus=1):
     # Specify hard-coded script features
     jobname = "nanopore_map"
     walltime = "12:00:00"
@@ -142,20 +93,26 @@ def create_shell_script(cmdFile, numJobs, outputFileName="run_nanopore_map.sh"):
     
     # Setup the script's contents
     formatStr = """#!/bin/bash -l
-#PBS -N {0}
-#PBS -l walltime={1}
-#PBS -l mem={2}
-#PBS -l ncpus=1
-#PBS -J 1-{3}
+#PBS -N {jobname}
+#PBS -l walltime={walltime}
+#PBS -l mem={mem}
+#PBS -l ncpus={cpus}
+#PBS -J 1-{numJobs}
 
 cd $PBS_O_WORKDIR
 
-eval $(cat {4} | head -n ${{PBS_ARRAY_INDEX}} | tail -n 1)
+eval $(cat {cmdFile} | head -n ${{PBS_ARRAY_INDEX}} | tail -n 1)
 """
-
     # Write to file
     with open(outputFileName, "w") as fileOut:
-        fileOut.write(formatStr.format(jobname, walltime, mem, numJobs, cmdFile))
+        fileOut.write(formatStr.format(
+            jobname=jobname,
+            walltime=walltime,
+            mem=mem,
+            cpus=cpus,
+            numJobs=numJobs,
+            cmdFile=cmdFile
+        ))
 
 def main():
     # User input
@@ -163,81 +120,117 @@ def main():
     tags for downstream popgen analysis of Nanopore reads. It will output a shell script
     amenable to batched job submission via PBS.
     
-    This script will also streamline any read filtration using Nanofilt or Chopper which can
-    be useful when dealing with amplicon-based variant calling.
+    The --prefix column must contain a unique value that identifies each sample's read files
+    (forward and reverse). This is used solely to locate the files in the -d directory.
     
-    You must provide the minimap2 executable location as an input. If it's in your system's
-    PATH, then providing "-m minimap2" will suffice. The same applies to NanoFilt or
-    Chopper.
+    The --id column must contain unique values for each sample. This will dictate the output
+    SAM files and their embedded read group identifiers, which will dictate what the samples
+    are called in your VCF files.
+    
+    During variant calling, if two samples have the same --sample value, they may be pooled together.
+    You probably want this to be unique for each sample and be the same as your --id value unless
+    you have a good reason to pool samples.
     
     The species ID column should contain unique values for each sample. It should
-    also be the prefix to each FASTQ file! The genotype columns should, when concatenated,
+    also be the prefix to each FASTA/Q file! The genotype columns should, when concatenated,
     render a unique value for each sample as well.
     """
     p = argparse.ArgumentParser(description=usage)
     ## Required
-    p.add_argument("-d", dest="fastqDirectory",
+    p.add_argument("-d", dest="fastaqDirectory",
                    required=True,
-                   help="Input directory where fastq files reside")
+                   help="Input directory where FASTA/Q files reside")
     p.add_argument("-f", dest="fastaFile",
                    required=True,
                    help="Input genome fasta file")
-    p.add_argument("-csv", dest="metadataCsv",
+    p.add_argument("-csv", dest="metadataFile",
                    required=True,
-                   help="Input Metadata CSV file")
-    p.add_argument("-m", dest="minimap2",
+                   help="Input Metadata CSV or TSV file")
+    p.add_argument("-p", dest="preset",
                    required=True,
-                   help="""Input the full path to the minimap2 executable; if it 
-                   is in your system path, just specify minimap2 here""")
-    p.add_argument("-qc", dest="qcProgram",
-                   required=True,
-                   help="""Input the full path to the executable being used for QC;
-                   if it is in your system path, just specify the name here""")
-    p.add_argument("-id", dest="speciesIdCol",
+                   choices=["map-ont", "asm5", "asm10", "asm20",
+                            "lr:hq", "splice", "splice:hq", "ava-ont"],
+                   help="Specify the preset to use for minimap2")
+    ## Optional (locations)
+    p.add_argument("--minimap2", dest="minimap2",
                    required=False,
-                   help="Column name where species ID is located")
-    p.add_argument("-g", dest="genotypeCols", nargs="+",
-                   required=True,
-                   help="One or more columns to concatenate as a genotype name")
-    p.add_argument("--minLength", dest="minLength",
-                   type=int,
-                   required=True,
-                   help="Specify a minimum read length")
-    p.add_argument("--maxLength", dest="maxLength",
-                   type=int,
-                   required=True,
-                   help="Specify a maximum read length")
-    ## Optional
-    p.add_argument("-l", dest="library",
+                   help="""If minimap2 is not available in your PATH variable, input
+                   the full path to the minimap2 executable here""",
+                   default=None)
+    p.add_argument("--chopper", dest="chopper",
                    required=False,
-                   help="String to use for library e.g., 'lib1' by default",
-                   default="lib1")
-    p.add_argument("-u", dest="unit",
-                   required=False,
-                   help="String to use for unit e.g., 'unit1' by default",
-                   default="unit1")
-    p.add_argument("--fqSuffix", dest="fqSuffix",
+                   help="""If chopper is not available in your PATH variable, input
+                   the full path to the chopper executable here""",
+                   default=None)
+    p.add_argument("--readsSuffix", dest="readsSuffix",
                    required=False,
                    help="""Specify the file suffix which ALL your FASTQ files have
                    e.g., '.fq.gz' by default""",
                    default=".fq.gz")
+    ## Optional (behavioural)
+    p.add_argument("--runChopper", dest="runChopper",
+                   required=False,
+                   action="store_true",
+                   help="""Set this flag if you want to run chopper on the FASTA/Q files""",
+                   default=False)
+    p.add_argument("--minLength", dest="minLength",
+                   type=int,
+                   required=False,
+                   help="Optionally, specify a minimum read length if --runChopper is set",
+                   default=-1)
+    p.add_argument("--maxLength", dest="maxLength",
+                   type=int,
+                   required=False,
+                   help="Optionally, specify a maximum read length if --runChopper is set",
+                   default=-1)
+    ## Optional (metadata)
+    p.add_argument("--prefix", dest="prefixCol",
+                   required=False,
+                   help="Column name where file prefix is located (default == 'prefix')",
+                   default="prefix")
+    p.add_argument("--id", dest="idCol",
+                   required=False,
+                   help="Column name where species ID is located (default == 'id')",
+                   default="id")
+    p.add_argument("--sample", dest="sampleCol",
+                   required=False,
+                   help="Column name where sample ID is located (default == 'sm')",
+                   default="sm")
+    p.add_argument("--platform", dest="platform",
+                   required=False,
+                   choices=["illumina", "pacbio", "nanopore"], #incomplete list
+                   help="String to use for readgroup platform e.g., 'illumina' by default",
+                   default="illumina")
+    p.add_argument("--library", dest="library",
+                   required=False,
+                   help="String to use for library e.g., 'lib1' by default",
+                   default="lib1")
+    p.add_argument("--unit", dest="unit",
+                   required=False,
+                   help="String to use for unit e.g., 'unit1' by default",
+                   default="unit1")
+    
     args = p.parse_args()
     validate_args(args)
     
     # Parse CSV file columns
-    speciesIds, genotypes = parse_metadata_csv(args.metadataCsv, args.speciesIdCol, args.genotypeCols)
+    prefixes, ids, samples = parse_metadata_csv(args.metadataFile, args.prefixCol, args.idCol, args.sampleCol)
     
-    # Process for readgroups and fqfiles
-    readgroups = process_readgroups(speciesIds, genotypes, args.library, args.unit)
+    # Validate that files exist and get their locations
+    fastqFiles = get_files_from_prefix(args.fastaqDirectory, prefixes)
+    
+    # Create readgroups
+    readgroups = process_readgroups(ids, samples, args.platform, args.library, args.unit)
     
     # Create cmd file
     cmdFileName = "cmd_nanopore_map.txt"
-    create_cmd_file(speciesIds, args.fastqDirectory, readgroups,
-                    args.fastaFile, args.minimap2, args.qcProgram,
-                    args.minLength, args.maxLength, cmdFileName, args.fqSuffix)
+    create_cmd_file(speciesIds, args.fastaqDirectory, readgroups, args.fastaFile,
+                    args.minimap2, args.preset, args.chopper, args.runChopper,
+                    args.minLength, args.maxLength, cmdFileName,
+                    args.readsSuffix, args.cpus)
     
     # Create shell script
-    create_shell_script(cmdFileName, len(readgroups))
+    create_shell_script(cmdFileName, len(readgroups), cpus=args.cpus)
     
     print("Program completed successfully!")
 
