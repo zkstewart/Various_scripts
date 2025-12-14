2f5b75f0ed4161becadbc275d0b2b5de96864008
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2025-12-14 21:18:52 +1000

    Update run_dorado_correction.sh
    Dorado version update, and simplify script with relative paths so there is less manual variable settings

--- basecalling/run_dorado_correction.sh
+++ basecalling/run_dorado_correction.sh
@@ -1,19 +1,20 @@
 #!/bin/bash -l
 
-JOBPREFIX=roberts_sample5
+JOBPREFIX=18Q053
 NUMBLOCKS=7
+MINREADLEN=5000
 
 ####
 
 # Specify the location of the dorado bin and lib folders
-DORADODIR=/home/stewarz2/various_programs/dorado/dorado-0.9.0-linux-x64
+DORADODIR=/home/stewarz2/various_programs/dorado/dorado-1.3.0-linux-x64
 
 # Specify the location of the Various_scripts folder
 VARSCRIPTDIR=/home/stewarz2/scripts/Various_scripts 
 
 # Specify FASTQ file for correction
-FQDIR=/work/ePGL/resequencing/nanopore/glauca_shearing_tests/Pete_Roberts_sample5__08_10_2024/fastq
-FQFILE=glauca_shearing_roberts_sample5.min5000.fastq
+FQDIR=fastq
+FQFILE=${JOBPREFIX}.min${MINREADLEN}.fastq
 
 # Specify output directory
 OUTDIR=fastq_corrected
