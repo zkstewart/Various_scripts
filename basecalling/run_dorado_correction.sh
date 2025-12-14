b63b24536716c8c7f417294010b265901337e7f4
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2025-12-14 21:28:17 +1000

    More improvements
    This script is now suited for afterok job submission as the prior steps emit variables to consistent file locations. So we should make it a proper qsub script

--- basecalling/run_dorado_correction.sh
+++ basecalling/run_dorado_correction.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -l
+#PBS -N subcorr
+#PBS -l walltime=00:10:00
+#PBS -l mem=10G
+#PBS -l ncpus=1
+#PBS -W depend=afterok:
 
-JOBPREFIX=18Q053
-MINREADLEN=5000
+cd $PBS_O_WORKDIR
 
 ####
+
+# Specify parameters for this sample / basecalling run
+PREFIX=18Q053
+MINREADLEN=5000
 
 # Specify the location of the dorado bin and lib folders
 DORADODIR=/home/stewarz2/various_programs/dorado/dorado-1.3.0-linux-x64
@@ -11,18 +19,18 @@ DORADODIR=/home/stewarz2/various_programs/dorado/dorado-1.3.0-linux-x64
 # Specify the location of the Various_scripts folder
 VARSCRIPTDIR=/home/stewarz2/scripts/Various_scripts 
 
-# Specify FASTQ file for correction
+# Specify input and output read file locations
 FQDIR=fastq
-FQFILE=${JOBPREFIX}.min${MINREADLEN}.fastq
-
-# Specify output directory
 OUTDIR=fastq_corrected
 
 ####
 
+# STEP 0: Derive file locations (following on from run_dorado_basecaller.sh)
+FQFILE=${PREFIX}.min${MINREADLEN}.fastq
 NUMBLOCKS=$(cat ${FQDIR}/numblocks.txt)
 
+# STEP 1: Run the pipeliner script
 python ${VARSCRIPTDIR}/basecalling/dorado_correct_pipeline.py -f ${FQDIR}/${FQFILE} \
     -n ${NUMBLOCKS} -o ${OUTDIR} \
     --dorado ${DORADODIR}/bin/dorado \
-    --jobPrefix ${JOBPREFIX} --memCPU 650G --walltimeCPU 48:00:00 --cpuCPU 22 --walltimeGPU 16:00:00 --memGPU 60G
+    --jobPrefix ${PREFIX} --memCPU 650G --walltimeCPU 48:00:00 --cpuCPU 22 --walltimeGPU 16:00:00 --memGPU 60G
