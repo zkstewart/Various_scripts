35d39b404c8f07f852bb24282791b71bec79cc29
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2026-04-14 18:52:57 +1000

    run_faststructure script interface improvement
    This should be a bit more intuitive

--- popgen/fastSTRUCTURE/run_faststructure.sh
+++ popgen/fastSTRUCTURE/run_faststructure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #PBS -N fstructure
 #PBS -l walltime=24:00:00
-#PBS -l mem=50G
+#PBS -l mem=20G
 #PBS -l ncpus=1
 #PBS -J 1-15
 
@@ -11,19 +11,18 @@ conda activate faststructure
 
 ####
 
-# Specify the PLINK2 BED file prefix
-## There should be three files like: ${BEDPREFIX}.bed, ${BEDPREFIX}.bim, ${BEDPREFIX}.fam
-BEDPREFIX=/mnt/f/lab_members/devindee/reformatted_variants/flgenomics_commercial
+# Specify the location of the PLINK2 BED files
+BEDDIR=/home/stewarz2/citrus/devindee/reformatted_variants
 
-# Specify the prefix for output files
+# Specify the prefix for input and output files
 PREFIX=flgenomics_commercial
 
+## Note: there should be three files within ${BEDDIR} which look like: ${PREFIX}.bed, ${PREFIX}.bim, ${PREFIX}.fam
 ####
 
-SEED=100 # no need to change, a constant seed makes the analysis replicable
-
 # STEP 1: Run structure.py with various K values
+SEED=100 # no need to change, a constant seed makes the analysis replicable
 structure.py -K ${PBS_ARRAY_INDEX} \
-    --input=${BEDPREFIX} --format=bed \
+    --input=${BEDDIR}/${PREFIX} --format=bed \
     --output=${PREFIX}_simple \
     --full --seed=${SEED}
