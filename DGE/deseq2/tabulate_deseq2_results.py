bcfeaf0b58146b80b2a788319446df8d38506943
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2026-05-03 18:44:49 +1000

    Create tabulate_deseq2_results.py
    Script to automatically produce the big DESeq2 results table that I used to make with a stupid amount of labour.

---
+++ DGE/deseq2/tabulate_deseq2_results.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+# tabulate_deseq2_results.py
+
+import os, argparse
+import pandas as pd
+from statistics import median
+
+class FileExistsError(Exception):
+    pass
+
+def validate_args(args):
+    # Validate DESeq2 file locations depending on type of input
+    args.files = []
+    for inputLocation in args.inputLocations:
+        inputLocation = os.path.abspath(inputLocation)
+        
+        # Handle directories
+        if os.path.isdir(inputLocation):
+            foundFiles = False
+            for f in os.listdir(inputLocation):
+                file = os.path.join(inputLocation, f)
+                if os.path.isfile(file) and file.endswith(args.inputSuffix):
+                    args.files.append(file)
+                    foundFiles = True
+            if not foundFiles:
+                raise FileNotFoundError(f"'{inputLocation}' does not contain any files.")
+        
+        # Handle files
+        elif os.path.isfile(inputLocation): # don't need to check suffix here
+            args.files.append(inputLocation)
+        else:
+            raise FileNotFoundError(f"'{inputLocation}' is not a valid file or directory.")
+    
+    # Validate coldata and normalised count file locations
+    args.coldataFileName = os.path.abspath(args.coldataFileName)
+    if not os.path.isfile(args.coldataFileName):
+        raise FileNotFoundError(f"-c '{args.outputFileName}' is not a file or does not exist!")
+    
+    args.normalisedCountsFileName = os.path.abspath(args.normalisedCountsFileName)
+    if not os.path.isfile(args.normalisedCountsFileName):
+        raise FileNotFoundError(f"-n '{args.normalisedCountsFileName}' is not a file or does not exist!")
+    
+    # Validate output file location
+    args.outputFileName = os.path.abspath(args.outputFileName)
+    if os.path.exists(args.outputFileName):
+        raise FileExistsError(f"The -o output location '{args.outputFileName}' already exists and will not be overwritten!")
+    
+    parentDir = os.path.dirname(args.outputFileName)
+    if not os.path.isdir(parentDir):
+        raise ValueError(f"The -o output location '{args.outputFileName}' points to a non-existent parent directory '{parentDir}'")
+
+def parse_coldata_file(fileName, groupingVariables):
+    sampleGroups = {}
+    with open(fileName, "r") as fileIn:
+        firstLine = True
+        for line in fileIn:
+            sl = line.rstrip().split("\t")
+            if firstLine:
+                header = sl
+                groupIndices = []
+                for variable in groupingVariables:
+                    try:
+                        groupIndices.append(header.index(variable))
+                    except:
+                        raise ValueError(f"-g value '{variable}' does not occur in the coldata header ({header})")
+                firstLine = False
+            else:
+                group = "-".join([ sl[i] for i in groupIndices ])
+                sampleGroups.setdefault(group, set())
+                sampleGroups[group].add(sl[0]) # first column is expected to be the sample
+    return sampleGroups
+
+def parse_deseq2_file(fileName, fileSuffix):
+    IGNORE = ["baseMean", "log2FoldChange", "lfcSE", "stat", "pvalue", "padj"]
+    
+    # Derive the comparison name
+    comparison = os.path.basename(fileName).rsplit(fileSuffix, maxsplit=1)[0]
+    
+    # Parse the file
+    d = {}
+    with open(fileName, "r") as fileIn:
+        firstLine = True
+        for line in fileIn:
+            sl = line.rstrip().split("\t")
+            if firstLine:
+                header = sl
+                keys = [ (x, i) for i, x in enumerate(header) if ( i != 0 ) and ( not x in IGNORE ) ]
+                firstLine = False
+            else:
+                geneID = sl[0]
+                d[geneID] = { x:sl[i] for x, i in keys }
+    return d, comparison
+
+def parse_normalised_counts(fileName, sampleGroups, toParse=None):
+    # Identify all samples that may be found within the counts file
+    samples = set()
+    for sgroup in sampleGroups.values():
+        samples.update(sgroup)
+    
+    # Parse counts file into a dictionary for later DataFrame generation
+    counts = {} # for later conversion into df
+    with open(fileName, "r") as fileIn:
+        firstLine = True
+        for line in fileIn:
+            sl = line.rstrip().split("\t")
+            if firstLine:
+                header = sl
+                sampleIndices = {
+                    s:header.index(s)
+                    for s in samples
+                    if s in header
+                }
+                firstLine = False
+            else:
+                geneID = sl[0]
+                counts[geneID] = {}
+                
+                # Skip this gene if we don't need to parse it
+                if toParse != None and (not geneID in toParse):
+                    continue
+                
+                # Obtain and store the median of this group's expression
+                for group, sgroup in sampleGroups.items():
+                    groupCounts = []
+                    for s in sgroup:
+                        if s in sampleIndices:
+                            groupCounts.append(float(sl[sampleIndices[s]]))
+                    counts[geneID][group] = median(groupCounts)
+    
+    # Convert dict into pandas DataFrame
+    countsDf = pd.DataFrame.from_dict(counts, orient="index")
+    
+    return countsDf
+
+def main():
+    ##### USER INPUT SECTION
+    usage = """%(prog)s will modify DESeq2 output files to include their best BLAST
+    name as found within a standard ZKS format annotation table (e.g., as produced by
+    BINge). Existing files will NOT be overwritten, and warnings will be emitted where
+    that is enforced.
+    """
+    # Required arguments
+    p = argparse.ArgumentParser(description=usage)
+    p.add_argument("-i", dest="inputLocations",
+                   required=True,
+                   nargs="+",
+                   help="Location of files or directories for DESeq2 results files to collate")
+    p.add_argument("-c", dest="coldataFileName",
+                   required=True,
+                   help="Location of the coldata file used by DESeq2")
+    p.add_argument("-g", dest="groupingVariables",
+                   required=True,
+                   nargs="+",
+                   help="One or more coldata columns to identify biological replicates")
+    p.add_argument("-n", dest="normalisedCountsFileName",
+                   required=True,
+                   help="Location of the normalised counts produced by DESeq2")
+    p.add_argument("-o", dest="outputFileName",
+                   required=True,
+                   help="Location to write the output TSV file")
+    # Optional arguments
+    p.add_argument("--is", dest="inputSuffix",
+                   required=False,
+                   nargs="+",
+                   help="""Optionally, specify the file ending of the DESeq2 output files
+                   if a directory is given to -i (default='.edit.tsv')""",
+                   default=".edit.tsv")
+    p.add_argument("--is", dest="inputSuffix",
+                   required=False,
+                   help="""Optionally, specify the file ending of the DESeq2 output files
+                   if a directory is given to -i (default='.edit.tsv')""",
+                   default=".edit.tsv")
+    
+    args = p.parse_args()
+    validate_args(args)
+    
+    # Parse coldata to derive the way to group samples for expression averaging
+    sampleGroups = parse_coldata_file(args.coldataFileName, args.groupingVariables)
+    
+    # Parse each DGE result file
+    genes = {}
+    comparisons = {}
+    for fileName in args.files:
+        genesDict, comparison = parse_deseq2_file(fileName, args.inputSuffix)
+        genes.update(genesDict)
+        comparisons[comparison] = set(genesDict.keys())
+    
+    # Format a dictionary for df conversion
+    for comparison, geneSet in comparisons.items():
+        for gene, geneDict in genes.items():
+            genes[gene][comparison] = "Y" if gene in geneSet else "."
+    df = pd.DataFrame.from_dict(genes, orient="index")
+    
+    # Parse the normalised counts
+    countsDf = parse_normalised_counts(args.normalisedCountsFileName, sampleGroups, set(genes.keys()))
+    
+    # Join the DGE and counts dataframes together
+    df = df.join(countsDf)
+    
+    # Write to file
+    df.to_csv(args.outputFileName, sep="\t")
+    
+    # Notify user of successful completion
+    print("Program completed successfully!")
+
+if __name__ == "__main__":
+    main()
