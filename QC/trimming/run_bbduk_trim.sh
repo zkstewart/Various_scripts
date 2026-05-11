f2e95381c94ffe943e2bd551e4a87863479c6cbf
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2026-05-11 19:15:35 +1000

    run_bbduk_trim easy resume failed samples

--- QC/trimming/run_bbduk_trim.sh
+++ QC/trimming/run_bbduk_trim.sh
@@ -3,19 +3,16 @@
 #PBS -l walltime=06:00:00
 #PBS -l mem=35G
 #PBS -l ncpus=4
-#PBS -J 1-2
+#PBS -J 1-X
 
 cd $PBS_O_WORKDIR
 
-module load Java/17.0.6
+conda activate readqc # conda create -n readqc bioconda::bbmap
 
 ####
 
-# Specify bbmap location
-BBDIR=/home/stewarz2/various_programs/bbmap
-
 # Specify reads dir
-READSDIR=/home/stewarz2/flies/mitch/prepared_reads
+READSDIR=/work/ePGL/sequencing/dna/illumina/citrus/COY18436_Dyneale/prepared_reads
 R1SUFFIX=_1.fq.gz
 R2SUFFIX=_2.fq.gz
 
@@ -23,6 +20,9 @@ R2SUFFIX=_2.fq.gz
 CPUS=4
 
 ####
+
+# STEP 0: Locate the adapters.fa file
+ADAPTERS=$(find ~/anaconda3/envs/readqc -type f -path "*/bbmap/resources/adapters.fa")
 
 # STEP 1: Find file prefixes
 declare -a RNAFILES
@@ -38,7 +38,9 @@ FILEPREFIX=${RNAFILES[${ARRAY_INDEX}]}
 BASEPREFIX=$(basename ${FILEPREFIX})
 
 # STEP 3: Run bbduk for trimming
-${BBDIR}/bbduk.sh -Xmx10g in1=${FILEPREFIX}${R1SUFFIX} in2=${FILEPREFIX}${R2SUFFIX} \
-	out1=${BASEPREFIX}.trimmed_1P.fq.gz out2=${BASEPREFIX}.trimmed_2P.fq.gz \
-	ref=${BBDIR}/resources/adapters.fa threads=${CPUS} \
-	ktrim=r k=23 mink=11 hdist=1 qtrim=rl trimq=5 minlength=25 tpe tbo
+if [[ ! -f ${BASEPREFIX}.ok ]]; then
+    bbduk.sh -Xmx10g in1=${FILEPREFIX}${R1SUFFIX} in2=${FILEPREFIX}${R2SUFFIX} \
+        out1=${BASEPREFIX}.trimmed_1P.fq.gz out2=${BASEPREFIX}.trimmed_2P.fq.gz \
+        ref=${ADAPTERS} threads=${CPUS} \
+        ktrim=r k=23 mink=11 hdist=1 qtrim=rl trimq=5 minlength=25 tpe tbo && touch ${BASEPREFIX}.ok;
+fi;
