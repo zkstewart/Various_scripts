cb06263f436b280b05b9df49754ca51ce973c4b5
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2026-07-08 18:06:03 +1000

    Create run_bwa_paired.hic.sh
    BWA MEM with Hi-C mapping parameterisation

---
+++ mapping/illumina/run_bwa_paired.hic.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -l
+#PBS -N bwamem
+#PBS -l walltime=12:00:00
+#PBS -l mem=55G
+#PBS -l ncpus=8
+#PBS -J 1-X
+
+cd $PBS_O_WORKDIR
+
+####
+
+# Specify reference genome FASTA location
+GENOMEFASTA=/work/ePGL/genomes/citrus/murcott/henry_upuli_2025/murcott_hap1.fasta
+
+# Specify reads dir
+READSDIR=/scratch/stewarz2/phase_variants/trimmed_hic
+R1SUFFIX=.trimmed_1P.fq.gz
+R2SUFFIX=.trimmed_2P.fq.gz
+
+# Specify computational parameters
+CPUS=8
+
+####
+
+# STEP 1: Find read file prefixes
+declare -a READFILES
+i=0
+for f in ${READSDIR}/*${R1SUFFIX}; do
+    READFILES[${i}]=$(echo "${f%%${R1SUFFIX}}");
+    i=$((i+1));
+done
+
+# STEP 2: Get job details
+ARRAY_INDEX=$((${PBS_ARRAY_INDEX}-1))
+FILEPREFIX=${READFILES[${ARRAY_INDEX}]}
+BASEPREFIX=$(basename ${FILEPREFIX})
+
+# STEP 3: Format readgroup
+RG="'@RG\\tID:${BASEPREFIX}\\tSM:${BASEPREFIX}\\tPL:illumina\\tLB:lib1\\tPU:unit1'"
+
+# STEP 4: Run bwa mem
+cmd=$(echo "bwa mem -t ${CPUS} -5SP -R $(echo ${RG}) ${GENOMEFASTA} ${FILEPREFIX}${R1SUFFIX} ${FILEPREFIX}${R2SUFFIX} > ${BASEPREFIX}.sam")
+eval $cmd
