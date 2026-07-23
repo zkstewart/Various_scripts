af7022fdfcdf8e61a6cdb8e80e8dde70e63f18b5
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2026-07-23 14:42:27 +1000

    salmon script updates

--- mapping/rnaseq/run_salmon.sh
+++ mapping/rnaseq/run_salmon.sh
@@ -1,34 +1,32 @@
 #!/bin/bash -l
-#PBS -N salmonM
-#PBS -l walltime=01:30:00
-#PBS -l mem=10G
+#PBS -N salmon
+#PBS -l walltime=02:30:00
+#PBS -l mem=15G
 #PBS -l ncpus=4
-#PBS -J 1-295
+#PBS -J 1-X
 
 cd $PBS_O_WORKDIR
 
-## MANUAL SETUP BELOW
+####
 
-# >> SETUP: Specify salmon executable location
+# Specify salmon executable location
 SALMONDIR=/home/stewarz2/various_programs/salmon/salmon-1.9.0_linux_x86_64/bin
 
-# >> SETUP: Specify salmon index location
-INDEXDIR=/home/stewarz2/plant_group/murcott/annotation
-INDEXFILE=hifiasm_OGSJun23_manual_26-09-23.trans_salmon_index
+# Specify salmon index location
+INDEXDIR=/work/ePGL/genomes/citrus/murcott/henry_upuli_2025/murcott_hap1.exon.fasta_salmon_index
 
-# >> SETUP: Specify reads location & file suffix
+# Specify reads location & file suffix
 ## For the suffix, it's assumed that just prior to the given string there
 ## is the 1 / 2 suffix differentiating forward / reverse reads
-READSDIR=/home/stewarz2/plant_group/murcott/bbduk
+READSDIR=/scratch/stewarz2/citrus/andrew_miles/dge_experiments/ngs713/trimmed_reads
 R1SUFFIX=.trimmed_1P.fq
 R2SUFFIX=.trimmed_2P.fq
 
-# >> SETUP: Specify computational resources
+# Specify computational resources
 CPUS=4
 
-## MANUAL SETUP END
+####
 
-## RUN PROGRAM
 # STEP 1: Locate all read prefixes for mapping
 declare -a RNAPREFIXES
 i=0
@@ -44,9 +42,8 @@ BASENAME=$(basename ${PREFIX})
 
 # STEP 3: Run mapping procedure for each sample
 ${SALMONDIR}/salmon quant --threads ${CPUS} \
-	--index ${INDEXDIR}/${INDEXFILE} \
-	--libType A \
-	-1 ${PREFIX}${R1SUFFIX} \
-	-2 ${PREFIX}${R2SUFFIX} \
-	-o ${BASENAME}.out
-
+                          --index ${INDEXDIR} \
+                          --libType A \
+                          -1 ${PREFIX}${R1SUFFIX} \
+                          -2 ${PREFIX}${R2SUFFIX} \
+                          -o ${BASENAME}.out
--- mapping/rnaseq/run_salmon_index.sh
+++ mapping/rnaseq/run_salmon_index.sh
@@ -6,22 +6,19 @@
 
 cd $PBS_O_WORKDIR
 
-## MANUAL SETUP BELOW
+####
 
-# >> SETUP: Specify salmon executable location
+# Specify salmon executable location
 SALMONDIR=/home/stewarz2/various_programs/salmon/salmon-1.9.0_linux_x86_64/bin
 
-# >> SETUP: Specify FASTA location
-FASTADIR=/home/stewarz2/banana_group/annotations
-FASTAFILE=Musa_acuminata_pahang_v4_gene.fna
+# Specify FASTA location
+FASTAFILE=murcott_hap1.exon.fasta
 
-# >> SETUP: Specify computational resources
+# Specify computational resources
 CPUS=8
 
-## MANUAL SETUP END
+####
 
-## RUN PROGRAM
-# STEP 1: Index the FASTA file where it resides
 ${SALMONDIR}/salmon index --threads ${CPUS} \
-	--transcripts ${FASTADIR}/${FASTAFILE} \
-	--index ${FASTADIR}/${FASTAFILE}_salmon_index
+                          --transcripts ${FASTAFILE} \
+                          --index ${FASTADIR}/${FASTAFILE}_salmon_index
